d92f9e75873b77c40fa50b7f97e473352093a86b
Vasantha Balla <vballa@codeaurora.org>
2017-12-13 15:22:31 +0530

    libstagefright: Fix issue in exactVideoFrame
    Video frame allocation and color conversion of frame
    is already added in O_MR1 aosp.So no need to do again.
    
    Change-Id: I38905789114e06e3ac19461d470b813943fa667d

--- media/libstagefright/StagefrightMetadataRetriever.cpp
+++ media/libstagefright/StagefrightMetadataRetriever.cpp
@@ -582,6 +582,15 @@ static VideoFrame *extractVideoFrame(
         }
     } while (err == OK && !done);
 
+    if (err != OK || size <= 0 || outputFormat == NULL) {
+        ALOGE("Failed to decode thumbnail frame");
+        source->stop();
+        decoder->release();
+        return NULL;
+    }
+
+    ALOGV("successfully decoded video frame.");
+
     if (thumbNailTime >= 0) {
         if (timeUs != thumbNailTime && outputFormat != NULL) {
             AString mime;
